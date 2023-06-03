c5d2ed285bd6bad821e7799dee172c9999f024b6
Maksim <75714743+Maksim8811@users.noreply.github.com>
2023-06-03 09:37:31 +0300

    add step 6

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import "./Contacts.css";
+import PropTypes from 'prop-types';
 
-function Contacts ({contacts}) {
-    
+const Contacts = ({contacts, onDelete}) => {
     
     return (
-    <>
+    <div className="container_contacts">
+    
     <ul>
         {contacts.map(({name, id, number}) => {
             return (
-                <li key={id}>{name}: {number}</li>
+                <li className="contacts_list" key={id}>{name}: {number}
+                <button className="contacts_list_button" type="button" onClick={()=>onDelete(id)}>Delete</button>
+                </li>
             )
         })}
     </ul>
-    </>
+    </div>
         )
+}
+
+Contacts.propTypes = {
+    contacts: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default Contacts
--- src/components/Phohebook/Phonebook.jsx
+++ src/components/Phohebook/Phonebook.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
-import "./Phonebook.css"
+import "./Phonebook.css";
 import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
 
 class Phonebook extends Component {
     loginInputId = nanoid()
@@ -16,7 +17,7 @@ class Phonebook extends Component {
       handleChange = evt => {
         const {name, value} = evt.target
         this.setState({[name]: value})
-        console.log('this.state', name, value)
+        
         
     }
 
@@ -35,19 +36,18 @@ class Phonebook extends Component {
         
         return (
         <>
-        
         <div className="container_form">
-       <form htmlFor={this.loginInputId} className="form" onSubmit={this.handleSubmit}>
+        <form htmlFor={this.loginInputId} className="form" onSubmit={this.handleSubmit}>
         <label className="label_form" id={this.loginInputId}>Name
-       <input
-  onChange={this.handleChange} 
-  value={this.state.name} 
-  className="input"     
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
+        <input
+        onChange={this.handleChange} 
+        value={this.state.name} 
+        className="input"     
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
 />
 
 <label className="label_form" id={this.telInputId}>Number
@@ -71,6 +71,10 @@ class Phonebook extends Component {
     
 )
 }
+}
+
+Phonebook.propTypes = {
+    onSubmit: PropTypes.func
 }
 
 export default Phonebook;
